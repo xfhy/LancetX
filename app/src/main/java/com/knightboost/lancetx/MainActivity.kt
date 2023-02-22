089c0998a8bc66761e14fc24cfeb29a91dcbf77f
xfhy <xfhy666@gmail.com>
2023-02-22 08:06:10 +0800

    监控Bitmap创建

--- app/src/main/java/com/knightboost/lancetx/MainActivity.kt
+++ app/src/main/java/com/knightboost/lancetx/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.knightboost.lancetx
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -14,10 +15,14 @@ class MainActivity : AppCompatActivity() {
         init_method_insert_test.setOnClickListener {
             ConstructorTest(" original call^");
         }
+        btn_create_bitmap.setOnClickListener {
+            val createBitmap = Bitmap.createBitmap(300, 600, Bitmap.Config.RGB_565)
+            Bitmap.createBitmap(310, 412, Bitmap.Config.RGB_565)
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i("Activity","onResume")
+        Log.i("Activity", "onResume")
     }
 }
--- app/src/main/java/com/knightboost/lancetx/weaver/ProxyTest.java
+++ app/src/main/java/com/knightboost/lancetx/weaver/ProxyTest.java
@@ -1,5 +1,9 @@
 package com.knightboost.lancetx.weaver;
 
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.util.Log;
+
 import com.knightboost.lancet.api.Origin;
 import com.knightboost.lancet.api.Scope;
 import com.knightboost.lancet.api.annotations.Group;
@@ -13,10 +17,31 @@ import com.knightboost.lancet.api.annotations.Weaver;
 public class ProxyTest {
 
     @Proxy()
-    @TargetClass(value = "android.util.Log",scope = Scope.SELF)
+    @TargetClass(value = "android.util.Log", scope = Scope.SELF)
     @TargetMethod(methodName = "i")
-    public static int replaceLogI(String tag,String msg){
+    public static int replaceLogI(String tag, String msg) {
         msg = msg + "lancet";
         return (int) Origin.call();
     }
+
+    @Proxy()
+    @TargetClass(value = "android.graphics.Bitmap", scope = Scope.ALL)
+    @TargetMethod(methodName = "createBitmap")
+    public static Bitmap createBitmap(int width, int height, Bitmap.Config config) {
+
+        //分析该Bitmap的大小
+        float factor = 1;
+        if (config.name().equals(Bitmap.Config.ARGB_8888.name())) {
+            factor = 4;
+        } else if (config.name().equals(Bitmap.Config.ARGB_4444.name()) || config.name().equals(Bitmap.Config.RGB_565.name())) {
+            factor = 2;
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && config.name().equals(Bitmap.Config.RGBA_F16.name())) {
+            factor = 8;
+        }
+        float size = width * height * factor / (1024f * 1024f);
+        Log.d("xfhy666", "这里在创建Bitmap size=" + size + "M");
+        //这里如果遇到size过大的可以打印堆栈
+        return (Bitmap) Origin.call();
+    }
+
 }
